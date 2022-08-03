6e55cdf853a1fd80494bfbe9f37cb82597a825b0
robertsamboy <106219442+robertsamboy@users.noreply.github.com>
2022-08-03 07:41:42 -0400

    Magic 8 Ball express application created

--- homework/server.js
+++ homework/server.js
@@ -4,6 +4,8 @@ const app = express();
 const port = process.env.PORT || 3003;
 
 
+const eightball = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
 app.get('/greeting/:name', (req,res)=>{
     res.send('how you doing today ' + req.params.name)
 })
@@ -14,6 +16,9 @@ app.get('/tip/:total/:tipPercentage', (req,res)=>{
 })
 
 
+app.get('/magic/:question/:eightballs', (req,res)=>{
+    res.send(req.params.question + '-----> '+ eightball[Math.floor(Math.random(req.params.eightballs)*20)+1])
+})
 
 
 
